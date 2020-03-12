5d48b3758095fefa179496a8491446238dbaaf09
tomytak <tomyt@hawaii.edu>
2020-03-12 13:32:15 -1000

    change

---
+++ README.md
@@ -0,0 +1,5 @@
+## npm demo thing with david
+David, aka santa claus, gave us all sorts of gits today. Thanks David!
+
+[View Demo on GH-Pages](https://kccnma.github.io/npm-demo-with-david)
+[View Demo on Netlify](https://npm-demo-with-david.netlify.com/)
--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,29 @@
-import { Parallax, Scroll } from 'giftbag'
-// All giftbag follow this pattern
+import {Parallax, Scroll} from 'giftbag'
+
+console.log(Parallax)
+
+// All Giftbag follow a pattern
 // 1. Create
 // 2. Setup
 // 3. Initialize
+
 // 1. Create new parallax
 const parallax = new Parallax();
-// 2. Setup
-// Selector elements
-const parallaxElements = document.querySelectorAll('.parallax-element')
+
+// 2. Setup parallax
+const parallaxElements = document.querySelectorAll('.parallax-element');
 parallax.setup({
-    selector: parallaxElements // This is the elements that will be selected
-})
+    selector: parallaxElements //these elements will be selected
+});
+
 // 3. Init
-parallax.init()
+parallax.init();
+
+
 // Scroll based animations
 const scroll = new Scroll();
 const scrollElements = document.querySelectorAll('.scroll-element');
 scroll.setup({
     selector: scrollElements
-})
+});
 scroll.init()
